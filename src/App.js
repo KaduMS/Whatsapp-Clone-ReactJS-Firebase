e05ceae3926b33326edc933628d1cf077201bcf4
Carlos Eduardo Machado de Souza <carlos.souza@pampa.compasso>
2021-04-19 18:57:13 -0300

    Fase inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="app-window">
+      <div className="sidebar">
+        
+        <header>
+          <img className="header--avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt="avatar" />
+          <div className="header--buttons">
+            ...
+          </div>
+        </header>
 
-export default App;
+        <div className="search">
+
+        </div>
+
+        <div className="chatlist">
+
+        </div>
+
+      </div>
+      <div className="contentarea">
+        ...
+      </div>
+    </div>
+  )
+}
